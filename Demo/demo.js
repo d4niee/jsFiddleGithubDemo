f2f7c5c88e00c53e67b0e72ff2f885fdd152fcdf
d4niee <daniellambrecht10@gmail.com>
2023-04-19 16:23:19 +0200

    Update demo.js

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,2 +1,2 @@
-import meistercharts from "https://skypack.dev/@meistercharts/meistercharts@1.31.0";
-const chart = meistercharts.createTimeLineChartFromId("timeLineChart");
+import meistercharts from "https://cdn.skypack.dev/@meistercharts/meistercharts@1.31.0";
+let chart = meistercharts.createTimeLineChartFromId("timeLineChart");
