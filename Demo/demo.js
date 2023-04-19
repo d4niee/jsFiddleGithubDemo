04a39f5e675273f08976f1f149bd4c9ba53fb693
d4niee <daniellambrecht10@gmail.com>
2023-04-19 17:40:51 +0200

    Update demo.js

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,5 +1,5 @@
 function handleButtonClick() {
-  let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    console.log('button clicked, new color: #', randomColor);
-  document.getElementById("heading").style.color = "#" + randomColor.toString();
+  let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+  console.log('button clicked, new color: ', randomColor);
+  document.getElementById("heading").style.color = randomColor.toString();
 }
