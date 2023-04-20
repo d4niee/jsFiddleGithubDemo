d3249f690c9cde85da8e4a35369eb64d8211c9f2
d4niee <daniellambrecht10@gmail.com>
2023-04-20 10:32:36 +0200

    Add files via upload

---
+++ meistercharts/examples/fiddles/minimal/meistercharts-minimal.js
@@ -0,0 +1,3 @@
+import meistercharts from "https://cdn.skypack.dev/@meistercharts/meistercharts@1.31.0";
+// create your first chart
+let chart = meistercharts.createTimeLineChartFromId("timeLineChart");
